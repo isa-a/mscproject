7d19388f4c696e75f2b941187860d5cc1a57a3f8
isa-a <65556208+isa-a@users.noreply.github.com>
2020-05-23 20:39:07 +0100

    Updated to fit a 3rd species

--- alternate method.py
+++ alternate method.py
@@ -7,22 +7,23 @@ Created on Sun May 17 06:29:00 2020
 """
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from numpy import *
 
 #creating function with the LV equations within it, first listing all parameters
-def lotkavolterra(t, X, K, rR, rC, alphaCR, alphaRC):
-     R, C = X
-     return [(rR*R)*((K - R- (alphaCR*C)) / K), (rC*C)*((K - C - (alphaRC*R)) / K)]
+def lotkavolterra(t, X, K, rR, rC, rC2, alphaCR, alphaRC, alphaC2R, alphaC2C):
+     R, C, C2 = X
+     return [(rR*R)*((K - R- (alphaCR*C)) / K), (rC*C)*((K - C - (alphaRC*R)) / K), (rC2*C2)*((K - C2 - (alphaC2R*C2) - (alphaC2C*C2)) / K)]
 
 
 
-solve = solve_ivp(lotkavolterra, [0, 15], [50, 500], args=(1000, 1, 1, 0.8, 0.8), dense_output=True) #passing through the values into the scipy solver
+solve = solve_ivp(lotkavolterra, [0, 100], [50, 200, 350], args=(1000, 1, 1, 1, 0.8, 0.8, 0.8, 0.46), dense_output=True) #passing through the values into the scipy solver
 
 
 
-t = linspace(0, 15, 50) #start and stop times and the no. of samples to generate
+t = linspace(0, 100, 100) #start and stop times and the no. of samples to generate
 X = solve.sol(t)
 plt.plot(t, X.T)
-plt.legend(['R', 'C'])
+plt.legend(['R', 'C','C2'])
 plt.title('Lotka-Volterra System')
 plt.show()
 
