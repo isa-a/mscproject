dc26ab2c8c8ffcea10232427f5677bee4b435f10
Isa <65556208+isa-a@users.noreply.github.com>
2020-08-15 14:03:52 +0100

    Latest

--- Data fitting/data.py
+++ Data fitting/data.py
@@ -16,6 +16,8 @@ import elfi
 import scipy.stats as ss
 from scipy.stats import poisson
 
+from SDEint import tauNspecies
+
 N = 2
 t = 21 # max time, starting from zero
 tau = 0.01
@@ -25,28 +27,36 @@ r = [0.39181, 0.2297] # growth rates
 Pops = np.array([[3.1713,	1.7697,	-0.007078,	1.2294,	-0.93122,	0.15738,	0.81653,	-0.010661,	0.067473], 
                              [0.17005,	-1.3495,	-0.048992, 	3.0277,	-1.5105,	-0.078494,	0.2635,	0.0036116,	-0.12046]])
 #len(Pops[1])
-measurementTimes = (([0,1,2,3,4,5,10,14,21]))
 
-m_idx = 0
-measurement_idx = []
-for t_idx, t in enumerate(measurementTimes):
-    if t >= measurementTimes[m_idx]:
-        measurement_idx.append(t_idx)
-        m_idx += 1
+alpha = array([[1, 0.18179], [0.37922, 1]])
 
-if len(measurement_idx) < len(measurementTimes):
-    sys.stderr.write("Not all measurement times found\n")
-
-Pops = np.array(Pops)
-times = np.array(measurementTimes)
-Pops = Pops[:, measurement_idx]
-times = times[measurement_idx]
+measurementTimes = np.array(([0,1,2,3,4,5,10,14,21]))
 
 
+simtimes, simpops = tauNspecies(t, X, N, K, r, alpha, tau)
+
+simpops = np.asanyarray([simpops])
+simpops = simpops.reshape(2101,2)
+#isinstance(simpops, list)
+
+def plotdata(simtimes, simpops):    
+
+    for index, pop in enumerate(simpops):
+        #print(ta)
+       # print(pop)
+        plt.plot(simtimes, simpops, label = 'Population '+str(index))
+    plt.legend()
+    
+    
+    plt.title('Stochastic System')
+    plt.xlabel('Time')
+    plt.ylabel('Population')
+    plt.legend(['Population 1', 'Population 2'])
+
+    plt.ylabel('Population')
+plotdata(simtimes, simpops)
 
 
-
-np.exp(1/-1.2124)
-np.exp(1/-0.055765)
+measurement_idx = np.array([int(x / tau) for x in measurementTimes])
 
 
