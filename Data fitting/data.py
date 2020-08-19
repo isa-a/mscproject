0104686bb63f9c94a32507f68cf2fcd4107ec12d
Isa <65556208+isa-a@users.noreply.github.com>
2020-08-19 16:36:16 +0100

    latest

--- Data fitting/data.py
+++ Data fitting/data.py
@@ -18,42 +18,39 @@ from scipy.stats import poisson
 
 from SDEint import tauNspecies
 
+#N = 2
+#t = 21*(10**5) # max time, starting from zero
+#tau = 0.01
+#X = [0.00015848*(10**5), 0.00017064*(10**5)]
+#K = [1.79*(10**5), 8.25*(10**5)] # carrying capacities
+#r = [0.39181*(10**5), 0.2297*(10**5)] # growth rates
+#Pops = np.array([[0.00015848, 0.00018786,	0.0000487,	0.0000464,	0.00095807,	0.00021153,	0.00014287,	0.0004099	, 0.00042039], 
+ #                            [0.00017064,	0.0040678,	0.023875,	0.023706,	0.081055,	0.031941,	0.070163,	1.8389,	1.7067]])
+
+
+
 N = 2
-t = 21 # max time, starting from zero
+t = 21# max time, starting from zero
 tau = 0.01
-X = [0.00015848*(10**11), 0.00017064*(10**11)]
-K = [1.79*(10**11), 8.25*(10**11)] # carrying capacities
-r = [0.39181*(10**11), 0.2297*(10**11)] # growth rates
-Pops = np.array([[3.1713,	1.7697,	-0.007078,	1.2294,	-0.93122,	0.15738,	0.81653,	-0.010661,	0.067473], 
-                             [0.17005,	-1.3495,	-0.048992, 	3.0277,	-1.5105,	-0.078494,	0.2635,	0.0036116,	-0.12046]])
-scale = (10**11)
-Pops = Pops*(10**11)
-#len(Pops[1])
+X = [0.00015848, 0.00017064]
+K = [1.79, 8.25] # carrying capacities
+r = [0.39181, 0.2297] # growth rates
+Pops = np.array([[0.00015848, 0.00018786,	0.0000487,	0.0000464,	0.00095807,	0.00021153,	0.00014287,	0.0004099	, 0.00042039], 
+                             [0.00017064,	0.0040678,	0.023875,	0.023706,	0.081055,	0.031941,	0.070163,	1.8389,	1.7067]])
+
+
+#Pops = np.array([[3.1713,	1.7697,	-0.007078,	1.2294,	-0.93122,	0.15738,	0.81653,	-0.010661,	0.067473], 
+  #                           []])
 
 alpha = np.array([[1, 0.18179], [0.37922, 1]])
 
 simtimes, simpops = tauNspecies(t, X, N, K, r, alpha, tau)
 #len(simpops[1])
-simpops = np.asanyarray([simpops])
-simpops = simpops.reshape(2101,2)
+#simpops = np.asanyarray([simpops])
+#simpops = simpops.reshape(2101,2)
 
-def plotdata(simtimes, simpops):    
-    for index, pop in enumerate(simpops):
-        #print(ta)
-       # print(pop)
-        plt.plot(simtimes, pop, label = 'Population '+str(index))
-    plt.legend()
-    
-    plt.title('Stochastic System')
-    plt.xlabel('Time')
-    plt.ylabel('Population')
-    plt.legend(['Population 1', 'Population 2'])
-
-    plt.ylabel('Population')
-plotdata(simtimes, simpops)
 
 measurementTimes = np.array(([0,1,2,3,4,5,10,14,21]))
-measurementTimes = measurementTimes*scale
 measurement_idx = np.array([int(x / tau) for x in measurementTimes])
 
 
